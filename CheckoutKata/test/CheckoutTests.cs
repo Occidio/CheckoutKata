6579243f534288b59993b3ce0f31011eeb140caf
Alex Bamford <Occidio001@gmail.com>
2019-07-08 19:36:56 +0100

    Test for multiple scan on same item.

--- CheckoutKata/test/CheckoutTests.cs
+++ CheckoutKata/test/CheckoutTests.cs
@@ -11,11 +11,30 @@ namespace CheckoutKata.test
         [TestCase("B", 30)]
         [TestCase("C", 20)]
         [TestCase("D", 15)]
-        public void Scan(string sku, int expectedPrice)
+        public void scan_once_single_item(string sku, int expectedPrice)
         {
             var checkout = new Checkout();
 
             checkout.Scan(sku);
+
+            var price = checkout.GetTotalPrice();
+
+            Assert.AreEqual(expectedPrice, price);
+        }
+
+        [Test]
+        [TestCase("A", 2, 100)]
+        [TestCase("B", 3, 90)]
+        [TestCase("C", 1, 20)]
+        [TestCase("D", 1, 15)]
+        public void scan_multiple_times_single_item(string sku, int amount, int expectedPrice)
+        {
+            var checkout = new Checkout();
+
+            for(int i = 0; i<amount; i++)
+            {
+                checkout.Scan(sku);
+            }
 
             var price = checkout.GetTotalPrice();
 
