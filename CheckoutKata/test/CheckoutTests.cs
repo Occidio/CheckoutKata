8360ae9673eb30e2f55676803b9c95ed3e9fdfc5
Alex Bamford <Occidio001@gmail.com>
2019-07-08 19:49:29 +0100

    Tests for discounts.

--- CheckoutKata/test/CheckoutTests.cs
+++ CheckoutKata/test/CheckoutTests.cs
@@ -25,13 +25,42 @@ namespace CheckoutKata.test
         [Test]
         [TestCase("A", 2, 100)]
         [TestCase("B", 3, 90)]
-        [TestCase("C", 1, 20)]
-        [TestCase("D", 1, 15)]
         public void scan_multiple_times_single_item(string sku, int amount, int expectedPrice)
         {
             var checkout = new Checkout();
 
             for(int i = 0; i<amount; i++)
+            {
+                checkout.Scan(sku);
+            }
+
+            var price = checkout.GetTotalPrice();
+
+            Assert.AreEqual(expectedPrice, price);
+        }
+
+        [Test]
+        public void scan_multiple_times_multiple_items()
+        {
+            var expectedPrice = 80;
+            var checkout = new Checkout();
+
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            var price = checkout.GetTotalPrice();
+
+            Assert.AreEqual(expectedPrice, price);
+        }
+
+        [Test]
+        [TestCase("A", 3, 130)]
+        [TestCase("B", 2, 45)]
+        public void scan_multiple_times_single_item_with_discount(string sku, int amount, int expectedPrice)
+        {
+            var checkout = new Checkout();
+
+            for (int i = 0; i < amount; i++)
             {
                 checkout.Scan(sku);
             }
