3543e83757b8ddeada56a520214427262c08957f
Alex Bamford <Occidio001@gmail.com>
2019-07-08 19:45:41 +0100

    Refactor checkout for multiple scans
    Replace arrays with lists.

--- CheckoutKata/src/Checkout.cs
+++ CheckoutKata/src/Checkout.cs
@@ -1,34 +1,37 @@
 ﻿
+using System.Collections.Generic;
+
 namespace CheckoutKata.src
 {
     class Checkout : ICheckout
     {
-        private Product[] availableProducts;
-        private Product scannedProduct;
+        private List<Product> availableProducts;
+        private List<Product> scannedProducts;
 
         public Checkout()
         {
-            availableProducts = new Product[] {
+            availableProducts = new List<Product>() {
                 new Product { SKU = "A", Price = 50 },
                 new Product { SKU = "B", Price = 30 },
                 new Product { SKU = "C", Price = 20 },
                 new Product { SKU = "D", Price = 15 }
             };
+
+            scannedProducts = new List<Product>();
+
         }
         public int GetTotalPrice()
         {
-            return scannedProduct.Price;
+            int price = 0;
+            foreach (Product product in scannedProducts){
+                price += product.Price;
+            }
+            return price;
         }
 
         public void Scan(string item)
         {
-            for (int i = 0; i < availableProducts.Length; i++)
-            {
-                if (availableProducts[i].SKU == item)
-                {
-                    scannedProduct = availableProducts[i];
-                }
-            }
+            scannedProducts.Add(availableProducts.Find((av) => av.SKU == item));
         }
     }
 }
